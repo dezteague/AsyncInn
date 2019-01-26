7bcf830617862f0a7fcba55c8e1dde1092eed1bc
Deziree' Teague <dez.teague@gmail.com>
2019-01-25 18:00:25 -0800

    created models with properties

---
+++ AsyncInn/AsyncInn/Models/Room.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Models
+{
+    public class Room
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public Layout RoomLayout { get; set; }
+    }
+
+    public enum Layout
+    {
+        Studio,
+        OneBedroom,
+        TwoBedroom
+    }
+}
