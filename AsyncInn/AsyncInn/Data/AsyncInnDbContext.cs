c0983620917e8a0458f71fd5036ff5ae02731bc1
Deziree' Teague <dez.teague@gmail.com>
2019-01-25 18:56:12 -0800

    added Db sets

--- AsyncInn/AsyncInn/Data/AsyncInnDbContext.cs
+++ AsyncInn/AsyncInn/Data/AsyncInnDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AsyncInn.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +9,20 @@ namespace AsyncInn.Data
 {
     public class AsyncInnDbContext : DbContext
     {
-        public AsyncInnDbContext(DbContextOptions<AsyncInnDbContext> options) : base(options);
+        public AsyncInnDbContext(DbContextOptions<AsyncInnDbContext> options) : base(options)
+        {
+
+        }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //composite key associations
+        }
+
+        public DbSet<Amenities> Amenities { get; set; }
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<HotelRoom> HotelRooms { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<RoomAmenities> RoomAmenities { get; set; }
     }
 }
