6d4986ec1b9bbc4c51e7c2986614b126a43ae528
Deziree' Teague <dez.teague@gmail.com>
2019-02-03 19:59:44 -0800

    added get/set tests for all models

--- AsyncInn/AsyncInnTest/UnitTest1.cs
+++ AsyncInn/AsyncInnTest/UnitTest1.cs
@@ -32,6 +32,83 @@ namespace AsyncInnTest
         }
 
         [Fact]
+        public void CanGetNameofRoom()
+        {
+            Room room = new Room();
+            room.Name = "Seattle Rain";
+
+            //can retrieve the name of a room
+            Assert.Equal("Seattle Rain", room.Name);
+        }
+
+        [Fact]
+        public void CanSetNameofRoom()
+        {
+            Room room = new Room();
+            room.Name = "Seattle";
+            room.Name = "Seattle Rain";
+
+            //can change the name of a room
+            Assert.Equal("Seattle Rain", room.Name);
+        }
+
+        [Fact]
+        public void CanGetNameofAmenity()
+        {
+            Amenities amenity = new Amenities();
+            amenity.Name = "HD TV";
+
+            //can retrieve the name of an amenity
+            Assert.Equal("HD TV", amenity.Name);
+        }
+
+        [Fact]
+        public void CanSetNameofAmenity()
+        {
+            Amenities amenity = new Amenities();
+            amenity.Name = "HD TV";
+            amenity.Name = "Hi-def TV";
+
+            //can change the name of an amenity
+            Assert.Equal("Hi-def TV", amenity.Name);
+        }
+
+        [Fact]
+        public void CanGetRateofHotelRoom()
+        {
+            HotelRoom hotelroom = new HotelRoom();
+            hotelroom.Rate = 100;
+
+            //can retrieve the rate of a hotel room
+            Assert.Equal(100, hotelroom.Rate);
+        }
+
+        [Fact]
+        public void CanSetRateofHotelRoom()
+        {
+            HotelRoom hotelroom = new HotelRoom();
+            hotelroom.Rate = 100;
+            hotelroom.Rate = 150;
+
+            //can change the rate of a hotel room 
+            Assert.Equal(150, hotelroom.Rate);
+        }
+
+        [Fact]
+        public void CanGetRoomAmenityID()
+        {
+            Room room = new Room();
+            room.ID = 1;
+            Amenities amenity = new Amenities();
+            amenity.ID = 1;
+
+            //can retrieve amenity id for a specific room
+            Assert.Equal(1, room.ID);
+            Assert.Equal(1, amenity.ID);
+        }
+
+
+        [Fact]
         public async void CanCreateHotel()
         {
             //testing hotel management service
